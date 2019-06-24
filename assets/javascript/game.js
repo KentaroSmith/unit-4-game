f748a0e3e5686090a152b04c545d99527f10e7fb
Dan Smith <daniel.smith8795@gmail.com>
2019-06-23 22:08:00 -0500

    added CSS rules to fit content within screen and adjust text colors

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,32 @@
 $(document).ready();
+//These objects are my four factions, all of thes values are placeholders
+var NCR = {
+    HP: 120,
+    attack: 6,
+    counter_attack: 20,
+}
+var BoS = {
+    HP: 120,
+    attack: 6,
+    counter_attack: 20,
+}
+var Khans = {
+    HP: 120,
+    attack: 6,
+    counter_attack: 20,
+}
+var Legion = {
+    HP: 120,
+    attack: 6,
+    counter_attack: 20,
+}
+//When the player clicks on the faction, I want to store that faction as "player"
+var player;
+//When we click on the faction we want to attack, I'm going to have that enemy stored as "defender"
+var defender;
+var enemies;
+
+function startgame() {
+
+
+}
